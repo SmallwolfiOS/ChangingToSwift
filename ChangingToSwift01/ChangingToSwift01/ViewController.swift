7d5d2cd550906ff4ffb8585843cbf9ace3df9dd2
mahaiping <1348748408@qq.com>
2016-05-25 22:44:53 +0800

    添加部分代码

--- ChangingToSwift01/ChangingToSwift01/ViewController.swift
+++ ChangingToSwift01/ChangingToSwift01/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var dataArray = ["0","1","2","3","4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.changeBackgroundColor()
     }
-
+    
+    func changeBackgroundColor() {
+        self.view.backgroundColor = UIColor.yellowColor()
+    }
+    
+    
+    //MARK:函数标签
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    //TODO:要做的函数标签
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    //FIXME:修复bug
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        cell.textLabel?.text = String.init(format: "这是第%d行", indexPath.row)
+        return cell
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
