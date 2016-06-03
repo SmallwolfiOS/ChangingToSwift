fda93731de9749d32295092c9ac45bb59dd33fba
Jason's iMac <1348748408@qq.com>
2016-06-03 20:03:25 +0800

    dsad

--- ChangingToSwift02/ChangingToSwift02/CameraViewController.swift
+++ ChangingToSwift02/ChangingToSwift02/CameraViewController.swift
@@ -7,20 +7,63 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class CameraViewController: UIViewController {
-
+class CameraViewController: UIViewController ,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    @IBOutlet weak var cameraBtn: UIButton!
+    var pickerVC:UIImagePickerController = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ininPickerViewController()
+        
+    }
+    //MARK:点击按钮的事件初始化
+    @IBAction func cameraAction(sender: UIButton) {
+        pickerVC.mediaTypes = [kUTTypeImage as String]//设定拍照的媒体类型
+        pickerVC.cameraCaptureMode = UIImagePickerControllerCameraCaptureMode.Photo //设置摄像头捕捉模式为捕捉图片
+        self .presentViewController(pickerVC, animated: true, completion: nil)
+    }
+    @IBAction func vedioBtnAction(sender: UIButton) {
+        
+    }
+    //MARK:初始化拾取控制器
+    func ininPickerViewController() {
+        pickerVC = UIImagePickerController.init() //继承自UINavigationController
+        pickerVC.sourceType = UIImagePickerControllerSourceType.Camera //设置拾取源为摄像头
+        pickerVC.cameraDevice = UIImagePickerControllerCameraDevice.Rear //设置摄像头为后置
+        pickerVC.editing = true;//设置运行编辑，即可以点击一些拾取控制器的控件
+        pickerVC.delegate = self//设置代理、
+//        pickerVC.modalPresentationStyle = UIModalPresentation;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //MARK:/* 拍照或录像成功，都会调用 */
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as!String//从info取出此时摄像头的媒体类型
+        if mediaType == kUTTypeImage as String{//如果是拍照
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            //保存图像到相簿
+            UIImageWriteToSavedPhotosAlbum(image, self, #selector(CameraViewController.image(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>){
+        if (error as NSError?) != nil {
+            print(error)
+        }else{
+            let ALertVC = UIAlertController.init(title: "保存成功", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+            
+            ALertVC.addAction(UIAlertAction.init(title: "确定", style: UIAlertActionStyle.Default, handler: nil))
+            presentViewController(ALertVC, animated: true, completion: nil)
+        }
+    }
+    func IKhandle()  {
+        
+    }
 
     /*
     // MARK: - Navigation
--- ChangingToSwift02/ChangingToSwift02/TableViewController.swift
+++ ChangingToSwift02/ChangingToSwift02/TableViewController.swift
@@ -40,18 +40,13 @@ class TableViewController: UITableViewController {
         return cell
     }
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        self .performSegueWithIdentifier("PushToDetail", sender: indexPath.row)
-    }
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "PushToDetail" {
-            let index = sender as! Int
-            let pushVC = segue.destinationViewController as! CameraViewController
-            if index == 0{
-                pushVC.title = 
-            }
+        let index = indexPath.row
+        if index == 1 {
+            self .performSegueWithIdentifier("PushToDetail", sender: indexPath.row)
         }
+        
     }
-    /*
+        /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
